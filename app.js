371b44e46d6ae9acc66d22cfd822ed000c3cd7cd
Steven Rejdukowski <steveo732@gmail.com>
2022-09-01 17:21:50 -0500

    fix in java for color function

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
- 'use strict';
+'use strict';
+let usercolor
+function askcolor() {
 
+     usercolor = prompt('What is your favorite color').toLowerCase();
+    let message;
 
- let usercolor = prompt('What is your favorite color');
- let message ;
+    if (usercolor == 'green') {
+        message = 'YES BEST COLOR'
+    } else if (usercolor == 'pink') {
+        message = "Oh yeah great choice";
+    } else if (usercolor == 'orange') {
+        message = "Third best in my book but still a great choice";
+    } else {
+        message = 'bad choice but its okay you are still welcome!';
 
- if (usercolor == 'Green'){
-     message = 'YES BEST COLOR'
- } else if (usercolor == 'Pink') {
-     message = "Oh yeah great choice";
- } else if (usercolor == 'Orange'){
-     message = "Third best in my book but still a great choice";
- } else {
-     message = 'bad choice but its okay you are still welcome!';
+    }
+    document.write(message);
      
- }
- document.write(message);
+}
+
+
+askcolor();
+
+
+
+function knowthings() {
+    document.write('Welcome to my page, I know your favorite color is ' + usercolor);
+}
+
+
 
 //  document.write('Welcome to my page, I know your favorite color is ' + usercolor);
 
