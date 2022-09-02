6e3e90b3218b33b91f4dd2df37fa8851c3cc404e
Steven Rejdukowski <steveo732@gmail.com>
2022-09-01 19:27:43 -0500

    added new java

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let usercolor
 function askcolor() {
 
-     usercolor = prompt('What is your favorite color').toLowerCase();
+    usercolor = prompt('What is your favorite color').toLowerCase();
     let message;
 
     if (usercolor == 'green') {
@@ -12,24 +12,45 @@ function askcolor() {
     } else if (usercolor == 'orange') {
         message = "Third best in my book but still a great choice";
     } else {
-        
+
         usercolor = prompt('Wrong favorite color, try again! Be careful though you only get one more chance to pick!!!').toLowerCase();
-        }
-        if (usercolor == 'green') {
-            message = 'YES BEST COLOR'
-        } else if (usercolor == 'pink') {
-            message = "Oh yeah great choice";
-        } else if (usercolor == 'orange') {
-            message = "Third best in my book but still a great choice";
-        } else {
-            message = 'bad choice but its okay you are still welcome!';
-        }
+    }
+    if (usercolor == 'green') {
+        message = 'YES BEST COLOR'
+    } else if (usercolor == 'pink') {
+        message = "Oh yeah great choice";
+    } else if (usercolor == 'orange') {
+        message = "Third best in my book but still a great choice";
+    } else {
+        message = 'bad choice but its okay you are still welcome!';
+    }
     document.write(message);
-     
 }
 
 
+
 askcolor();
+
+
+function guesshall() {
+    let answer;
+
+    while (answer != 'Hall and Oates') {
+        answer = prompt('Who was objectively and subjectively the best reocrindg artist of the 1980s? Hint, it rhymes with Call and Boats.');
+        if (answer != 'Hall and Oates') {
+            alert('Try again! Like what were you thinking?');
+        } else {
+            alert('You are correct! You are so smart and have great taste as well!');
+        }
+    }
+}
+
+guesshall();
+
+
+
+
+
 
 
 
@@ -43,4 +64,17 @@ function knowthings() {
 
 function myFunction() {
     document.getElementById("demo").innerHTML = "Thanks!";
+}
+
+
+
+function showrainbow() {
+
+    let output = '';
+    let rating = prompt('How many rainbows would you rate my page? 1-5');
+    for (let i = 0; i < rating; i++) {
+        output += "<img class='rainbow' src='images.png'/>";
+    }
+
+    return document.write(output);
 }
